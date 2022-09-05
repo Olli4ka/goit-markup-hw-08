435314a352f613924c23a9af2515bb35869989a1
Olga Panayot <panayot.olga@gmail.com>
2022-09-05 13:11:50 +0300

    menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,22 @@
 (() => {
 	const refs = {
-		openModalBtn: document.querySelector("[data-modal-open]"),
-		closeModalBtn: document.querySelector("[data-modal-close]"),
-		modal: document.querySelector("[data-modal]"),
+		openMenuBtn: document.querySelector("[data-menu-open]"),
+		closeMenuBtn: document.querySelector("[data-menu-close]"),
+		menu: document.querySelector("[data-menu]"),
 		body: document.querySelector("body"),
+		menuList: document.querySelector(".mob-menu-nav"),
 	};
 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+	refs.openMenuBtn.addEventListener("click", toggleMenu);
+	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+	refs.menuList.addEventListener("click", removeMenu);
 
-	function toggleModal() {
-		refs.modal.classList.toggle("is-hidden");
+	function toggleMenu() {
+		refs.menu.classList.toggle("is-hidden");
 		refs.body.classList.toggle("no-scroll");
+	}
+	function removeMenu() {
+		refs.menu.classList.add("is-hidden");
+		refs.body.classList.remove("no-scroll");
 	}
 })();
